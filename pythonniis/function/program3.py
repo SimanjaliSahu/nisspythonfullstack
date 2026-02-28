4030029858445523da7886b0ca6e91286a767b94
SimanjaliSahu <simanjalisahu2@gmail.com>
2026-02-28 10:40:52 +0530

    basicprogram

---
+++ pythonniis/function/program3.py
@@ -0,0 +1,10 @@
+def sical(p,t,r):
+	si=p*r*t/100
+	print("simpleinterset=",si)
+print("enter principle")
+p=float(input())
+print("enter rate of interset")
+r=(input())
+print("enter time")
+t=(input())
+sical(p,t,r)
